aa8a2f352571c361fe9ab168eacbb5516500a1de
Malouf Jarett <jem276@zoo.cs.yale.edu>
2021-11-03 19:21:24 -0400

    5 passing

--- process.c
+++ process.c
@@ -11,6 +11,7 @@
 #include <limits.h>
 #include <linux/limits.h>
 #include "/c/cs323/proj4/starter-code/parse.h"
+#include "process.h"
 
 #define STACK_SIZE 100
 
@@ -48,62 +49,136 @@ char *dir_peek() { return dir_stack[top]; }
 //   struct cmd *right;    // Right subtree or NULL (default)
 // } CMD;
 
+void handle_redirect_input(const CMD *cmd) {
+  int new_input_fd = open(cmd->fromFile, O_RDONLY);
+  dup2(new_input_fd, STDIN_FILENO);
+  close(new_input_fd);
+}
+
+void handle_redirect_here_input(const CMD *cmd) {
+  char template[] = "/tmp/Bash_heredoc_XXXXXX";
+  int tmp_fd = mkstemp(template);
+  write(tmp_fd, cmd->fromFile, strlen(cmd->fromFile));
+  lseek(tmp_fd, 0, SEEK_SET);
+  dup2(tmp_fd, STDIN_FILENO);
+  close(tmp_fd);
+}
+
+void handle_redirect_output_append(const CMD *cmd) {
+  int new_output_fd = open(cmd->toFile, O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
+  lseek(new_output_fd, 0, SEEK_END);
+  dup2(new_output_fd, STDOUT_FILENO);
+  close(new_output_fd);
+}
+
+void handle_redirect_output(const CMD *cmd) {
+  int new_output_fd = open(cmd->toFile, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+  dup2(new_output_fd, STDOUT_FILENO);
+  close(new_output_fd);
+}
+
+void handle_dir_change(const CMD *cmd) {
+  char *command = cmd->argv[0];
+  if (strcmp(command, "pushd") == 0) {
+    char *path = cmd->argv[cmd->argc - 1];
+    chdir(path);
+    dir_push(path);
+  } else if (strcmp(command, "popd") == 0) {
+    dir_pop();
+    chdir(dir_peek());
+  } else if (strcmp(command, "cd") == 0) {
+    char *path = cmd->argv[cmd->argc - 1];
+    chdir(path);
+  }
+}
+
+void handle_var_definitions(const CMD *cmd) {
+  for (int i = 0; i < cmd->nLocal; i++) {
+    setenv(cmd->locVar[i], cmd->locVal[i], 1);
+  }
+}
+
+void handle_redirects(const CMD *cmd) {
+  switch (cmd->toType) {
+    case RED_OUT_APP:
+      handle_redirect_output_append(cmd); break;
+    case RED_OUT:
+      handle_redirect_output(cmd); break;
+    default: break;
+  }
+
+  if (cmd->fromFile != NULL) {
+    switch (cmd->fromType) {
+      case RED_IN_HERE:
+        handle_redirect_here_input(cmd); break;
+      case RED_IN:
+        handle_redirect_input(cmd); break;
+      default: break;
+    }
+  }
+}
+
 int process_simple(const CMD *cmd) {
   int status;
-  char *command = cmd->argv[0];
   pid_t pid = fork();
 
   if (pid == 0) {
-    // handle var definition
-    for (int i = 0; i < cmd->nLocal; i++) {
-      fprintf(stderr, "processing %s\n", cmd->locVar[i]);
-      setenv(cmd->locVar[i], cmd->locVal[i], 1);
-    }
-
-    // handle redirects
-    if (cmd->toFile != NULL) {
-      int new_output_fd = open(cmd->toFile, O_RDWR);
-      dup2(new_output_fd, STDOUT_FILENO);
-      close(new_output_fd);
-    }
-
-    if (cmd->fromFile != NULL) {
-      int new_input_fd = open(cmd->fromFile, O_RDONLY);
-      dup2(new_input_fd, STDIN_FILENO);
-      close(new_input_fd);
-    }
-
-    // handle specific cases for pushd, popd, cd
-    if (strcmp(command, "pushd") == 0) {
-      char *path = cmd->argv[cmd->argc - 1];
-      chdir(path);
-      dir_push(path);
-    } else if (strcmp(command, "popd") == 0) {
-      dir_pop();
-      chdir(dir_peek());
-    } else if (strcmp(command, "cd") == 0) {
-      char *path = cmd->argv[cmd->argc - 1];
-      chdir(path);
-    }
-
-    execvp(command, cmd->argv);
+    handle_var_definitions(cmd);
+    handle_redirects(cmd);
+    handle_dir_change(cmd);
+    int exec_status = execvp(cmd->argv[0], cmd->argv);
+    if (exec_status == -1) exit(0);
   }
 
-  waitpid(pid, &status, WNOHANG | WUNTRACED | WCONTINUED);
+  waitpid(pid, &status, 0);
+  
+  return STATUS(status);
+}
 
-  return status;
+int process_pipe(const CMD *cmd) {
+  if (cmd == NULL) return 0;
+  int fd_pair[2];
+  int status;
+  pipe(fd_pair);
+  
+  int left_pid = fork();
+  if (left_pid == 0) {
+    dup2(fd_pair[1], STDOUT_FILENO);
+    status = process(cmd->left);
+    close(fd_pair[0]);
+    close(fd_pair[1]);
+    exit(status);
+  }
+  waitpid(left_pid, &status, 0);
+
+  int right_pid = fork();
+  if (right_pid == 0) {
+    dup2(fd_pair[0], STDIN_FILENO);
+    status = process(cmd->right);
+    close(fd_pair[0]);
+    close(fd_pair[1]);
+    exit(status);
+  }
+  waitpid(right_pid, &status, 0);
+
+  close(fd_pair[0]);
+  close(fd_pair[1]);
+
+  return STATUS(status);
 }
 
 int process(const CMD *cmd) {
   int status = 0;
   if (cmd == NULL) return status;
-  
+
   switch (cmd->type) {
+    case PIPE:
+      status = process_pipe(cmd);
     case SIMPLE:
     default:
-      status = status | process_simple(cmd);
+      status = process_simple(cmd);
   }
-  status = status | process(cmd->left);
-  status = status | process(cmd->right);
+  // status = status | process(cmd->left);
+  // status = status | process(cmd->right);
   return status;
 }
