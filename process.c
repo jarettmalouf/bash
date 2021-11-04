60dd8ee4077a194a1daddd8eb61022f6386d36e1
Malouf Jarett <jem276@zoo.cs.yale.edu>
2021-11-03 23:01:44 -0400

    Passing 11

--- process.c
+++ process.c
@@ -22,32 +22,11 @@ char *dir_pop() { return dir_stack[top--]; }
 void dir_push(char *path) { dir_stack[++top] = path; }
 char *dir_peek() { return dir_stack[top]; }
 
-// typedef struct cmd {
-//   int type;             // Node type: SIMPLE, PIPE, SEP_AND, SEP_OR, SEP_END,
-// 			//   SEP_BG, SUBCMD, or NONE (default)
-
-//   int argc;             // Number of command-line arguments
-//   char **argv;          // Null-terminated argument vector or NULL
-
-//   int nLocal;           // Number of local variable assignments
-//   char **locVar;        // Array of local variable names and the values to
-//   char **locVal;        //   assign to them when the command executes
-
-//   int fromType;         // Redirect stdin: NONE (default), RED_IN (<), or
-// 			//   RED_IN_HERE (<<)
-//   char *fromFile;       // File to redirect stdin, contents of here document,
-// 			//   or NULL (default)
-
-//   int toType;           // Redirect stdout: NONE (default), RED_OUT (>),
-// 			//   RED_OUT_APP (>>)
-//   char *toFile;         // File to redirect stdout or NULL (default)
-
-//   int errType;          // Unused for this project.
-//   char *errFile;        // Unused for this project.
-
-//   struct cmd *left;     // Left subtree or NULL (default)
-//   struct cmd *right;    // Right subtree or NULL (default)
-// } CMD;
+int get_exit_code(int s1, int s2) {
+  if (s2 != 0) return s2;
+  if (s1 != 0) return s1;
+  return 0;
+}
 
 void handle_redirect_input(const CMD *cmd) {
   int new_input_fd = open(cmd->fromFile, O_RDONLY);
@@ -107,14 +86,12 @@ void handle_redirects(const CMD *cmd) {
     default: break;
   }
 
-  if (cmd->fromFile != NULL) {
-    switch (cmd->fromType) {
-      case RED_IN_HERE:
-        handle_redirect_here_input(cmd); break;
-      case RED_IN:
-        handle_redirect_input(cmd); break;
-      default: break;
-    }
+  switch (cmd->fromType) {
+    case RED_IN_HERE:
+      handle_redirect_here_input(cmd); break;
+    case RED_IN:
+      handle_redirect_input(cmd); break;
+    default: break;
   }
 }
 
@@ -135,50 +112,212 @@ int process_simple(const CMD *cmd) {
   return STATUS(status);
 }
 
+void close_fd_pair(int fd_pair[2]) {
+  close(fd_pair[0]);
+  close(fd_pair[1]);
+}
+
 int process_pipe(const CMD *cmd) {
   if (cmd == NULL) return 0;
   int fd_pair[2];
-  int status;
+  int status_l, status_r;
   pipe(fd_pair);
   
-  int left_pid = fork();
+  pid_t left_pid = fork();
   if (left_pid == 0) {
     dup2(fd_pair[1], STDOUT_FILENO);
-    status = process(cmd->left);
-    close(fd_pair[0]);
-    close(fd_pair[1]);
-    exit(status);
+    close_fd_pair(fd_pair);
+    exit(process(cmd->left));
   }
-  waitpid(left_pid, &status, 0);
 
-  int right_pid = fork();
+  pid_t right_pid = fork();
   if (right_pid == 0) {
     dup2(fd_pair[0], STDIN_FILENO);
-    status = process(cmd->right);
-    close(fd_pair[0]);
-    close(fd_pair[1]);
-    exit(status);
+    close_fd_pair(fd_pair);
+    exit(process(cmd->right));
   }
-  waitpid(right_pid, &status, 0);
 
-  close(fd_pair[0]);
-  close(fd_pair[1]);
+  close_fd_pair(fd_pair);
 
+  waitpid(left_pid, &status_l, 0);
+  waitpid(right_pid, &status_r, 0);
+
+  return STATUS(get_exit_code(status_l, status_r));
+}
+
+void print_info(const CMD *cmd) {
+  if (cmd == NULL) return;
+  fprintf(stderr, "Here is the command struct:\n");
+  fprintf(stderr, "type: %d\n", cmd->type);
+  fprintf(stderr, "argc: %d\n", cmd->argc);
+  fprintf(stderr, "nLocal: %d\n", cmd->nLocal);
+  fprintf(stderr, "fromType: %d\n", cmd->fromType);
+  print_info(cmd->left);
+  print_info(cmd->right);
+}
+
+int process_sep_end(const CMD *cmd) {
+  int status_l, status_r;
+
+  pid_t left_pid = fork();
+  if (left_pid == 0) {
+    status_l = process(cmd->left);
+    exit(status_l);
+  }
+  waitpid(left_pid, &status_l, 0);
+
+  pid_t right_pid = fork();
+  if (right_pid == 0) {
+    status_r = process(cmd->right);
+    exit(status_r);
+  }
+  waitpid(right_pid, &status_r, 0);
+
+  return STATUS(get_exit_code(status_l, status_r));
+}
+
+int process_subcmd(const CMD *cmd) {
+  int status;
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    handle_var_definitions(cmd);
+    handle_redirects(cmd);
+    // handle_dir_change(cmd);
+    exit(process(cmd->left));
+  }
+
+  waitpid(pid, &status, 0);
+  
   return STATUS(status);
 }
 
+int process_sep_bg(const CMD *cmd) {
+  int status_r = 0;
+
+  pid_t left_pid = fork();
+  if (left_pid == 0) {
+    exit(process(cmd->left));
+  }
+
+  pid_t right_pid = fork();
+  if (right_pid == 0) {
+    status_r = process(cmd->right);
+    fprintf(stderr, "Backgrounded: %d\n", getpid());
+    exit(status_r);
+  }
+
+  waitpid(right_pid, &status_r, 0);
+  return STATUS(status_r);
+}
+
+void reap_zombies() {
+  int child_status;
+  pid_t pid;
+  while (1) {
+    pid = waitpid(-1, &child_status, WNOHANG);
+    if (pid <= 0) break;
+    fprintf(stderr, "Completed: %d (%d)\n", pid, child_status);
+  }
+}
+
+int process_sep_and(const CMD *cmd) {
+  int status_l, status_r;
+  
+  pid_t left_pid = fork();
+  if (left_pid == 0) {
+    status_l = process(cmd->left);
+    exit(status_l);
+  }
+  waitpid(left_pid, &status_l, 0);
+
+  if (status_l != 0) {
+    return STATUS(status_l);
+  }
+
+  pid_t right_pid = fork();
+  if (right_pid == 0) {
+    status_r = process(cmd->right);
+        exit(status_r);
+
+  }
+  waitpid(right_pid, &status_r, 0);
+
+  return STATUS(status_r);
+}
+
+int process_sep_or(const CMD *cmd) {
+  int status_l, status_r;
+  
+  pid_t left_pid = fork();
+  if (left_pid == 0) {
+    status_l = process(cmd->left);
+    exit(status_l);
+  }
+  waitpid(left_pid, &status_l, 0);
+
+  if (status_l == 0) {
+    return STATUS(status_l);
+  }
+
+  pid_t right_pid = fork();
+  if (right_pid == 0) {
+    status_r = process(cmd->right);
+    exit(status_r);
+  }
+  waitpid(right_pid, &status_r, 0);
+
+  return STATUS(status_r);
+}
+
 int process(const CMD *cmd) {
+  reap_zombies();
   int status = 0;
   if (cmd == NULL) return status;
 
   switch (cmd->type) {
+    case SEP_BG:
+      status = process_sep_bg(cmd); break;
+    case SEP_END:
+      status = process_sep_end(cmd); break;
     case PIPE:
-      status = process_pipe(cmd);
-    case SIMPLE:
+      status = process_pipe(cmd); break;
+    case SEP_AND:
+      status = process_sep_and(cmd); break;
+    case SEP_OR:
+      status = process_sep_or(cmd); break;
+    case SUBCMD:
+      status = process_subcmd(cmd); break;
     default:
       status = process_simple(cmd);
   }
-  // status = status | process(cmd->left);
-  // status = status | process(cmd->right);
+
   return status;
 }
+
+// typedef struct cmd {
+//   int type;             // Node type: SIMPLE, PIPE, SEP_AND, SEP_OR, SEP_END,
+// 			//   SEP_BG, SUBCMD, or NONE (default)
+
+//   int argc;             // Number of command-line arguments
+//   char **argv;          // Null-terminated argument vector or NULL
+
+//   int nLocal;           // Number of local variable assignments
+//   char **locVar;        // Array of local variable names and the values to
+//   char **locVal;        //   assign to them when the command executes
+
+//   int fromType;         // Redirect stdin: NONE (default), RED_IN (<), or
+// 			//   RED_IN_HERE (<<)
+//   char *fromFile;       // File to redirect stdin, contents of here document,
+// 			//   or NULL (default)
+
+//   int toType;           // Redirect stdout: NONE (default), RED_OUT (>),
+// 			//   RED_OUT_APP (>>)
+//   char *toFile;         // File to redirect stdout or NULL (default)
+
+//   int errType;          // Unused for this project.
+//   char *errFile;        // Unused for this project.
+
+//   struct cmd *left;     // Left subtree or NULL (default)
+//   struct cmd *right;    // Right subtree or NULL (default)
+// } CMD;
